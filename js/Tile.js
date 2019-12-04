fc9c25fe4e13ed3452b5f9b1a6ac40e71f6a16e5
DanielHansson <dh99@live.se>
2019-12-04 15:07:31 +0100

    Refactored texture Function

--- js/Tile.js
+++ js/Tile.js
@@ -1,20 +1,16 @@
-const size = 64;
+import { getTexture } from './utils.js'
 
 export default {
     template: `
         <div ref="tile" 
-        class="tile" 
-        @click="logPosition">
+        class="tile sprite" 
+        @click="setTileTexture">
             <h3></h3>
         </div>
         `,
     props: ['properties'],
     data() {
         return {
-            /*srcRect: { //position of the tileset, taken from XNA C# monogame development
-                x: 0,
-                y: 0
-            },*/
             tileId: this.properties.tileId
         }
     },
@@ -23,17 +19,24 @@ export default {
             console.log(this.properties.x, this.properties.y, this.properties.tileId);
         },
         setTileTexture() {
-            let pos = {x, y};
-            switch(tileId){ //Working on a system to read from tileset and set position in the image to be shown as a tile Image.
+            let pos;
+            switch(this.tileId){ //Working on a system to read from tileset and set position in the image to be shown as a tile Image.
                 case 0:
-                    return pos = {x: 32, y: 0};
+                    pos = {x: 1, y: 0};
+                    break;
                 case 1:
-                    return pos = {x: 64, y: 0};
+                    pos = {x: 2, y: 0};
+                    break;
             }
+            this.changeTexture(pos)
+        },
+        changeTexture(pos){
+            this.$refs.tile.style.backgroundPosition = getTexture(pos.x, pos.y);
         }
     },
     mounted() {
-        this.$refs.tile.style.setProperty('background-image', `url(${this.properties.image})`);
-        document.documentElement.style.setProperty('--size', size + "px"); //Sets a "--size" variable to be used in css style.css
+        //this.$refs.tile.style.setProperty('background-position', setTileTexture(tileId))
+        //this.$refs.tile.style.setProperty('background-image', `url(${this.properties.image})`);
+        document.documentElement.style.setProperty('--tile_size', 32 + "px"); //Sets a "--size" variable to be used in css style.css
     }
 }
