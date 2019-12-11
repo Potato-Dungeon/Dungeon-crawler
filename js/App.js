3ec25850ca437e5494e1a51e980913dcf1ea2bb1
Goldy <vikk@live.se>
2019-12-11 09:59:12 +0100

    Conflict resolution

--- js/App.js
+++ js/App.js
@@ -1,11 +1,14 @@
 import Grid from './Grid.js'
+import TileSelector from './TileSelector.js'
 
 export default {
     components: {
-        Grid
+        Grid,
+        TileSelector
     },
     template: `
     <div>
+        <TileSelector ref="tile-selector"></TileSelector>
         <grid></grid>
     </div>
     `
